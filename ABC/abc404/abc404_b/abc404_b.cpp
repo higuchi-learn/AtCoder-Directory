943c5ccc337f718995bc1c3f712d977f9b3c428c
higuchi-learn <haruki.higuchi.000@gmail.com>
2025-05-04 23:06:26 +0900

    404

---
+++ ABC/abc404/abc404_b/abc404_b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/dsu>
+#include <cstdio>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+#define rep0(i, n) for (ll i = 0; i < (n); i++)
+#define rep1(i, n) for (ll i = 1; i <= (n); i++)
+#define all(v) v.begin(), v.end()
+
+ll GCD(ll A, ll B) {
+  while (true) {
+    if (A >= B) A = A % B;
+    else
+      B = B % A;
+    if (A == 0 || B == 0) break;
+  }
+  if (A == 0) return B;
+  return A;
+}
+
+ll LCM(ll A, ll B) { return A / GCD(A, B) * B; }
+
+ll nCr(ll N, ll R) {
+  if (R > N) return 0;
+  if (R > N - R) R = N - R;
+  ll res = 1;
+  rep1(i, R) {
+    res *= N - R + i;
+    res /= i;
+  }
+  return res;
+}
+
+int main() {
+  ll N;
+  cin >> N;
+  
+}
